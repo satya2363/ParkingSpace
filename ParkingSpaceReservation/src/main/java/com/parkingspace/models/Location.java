4fba92c5680ea85ab319cab696cabc1a89fdb3d7
Tharunya Reddy <tharunyapati@gmail.com>
2020-12-22 14:40:59 -0500

    Adding lombok annotations and removing manual getters and setters

--- ParkingSpaceReservation/src/main/java/com/parkingspace/models/Location.java
+++ ParkingSpaceReservation/src/main/java/com/parkingspace/models/Location.java
@@ -7,85 +7,35 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Data
+@NoArgsConstructor
 public class Location implements Serializable {
+
     /**
-     * 
+     *
      */
     private static final long serialVersionUID = 1L;
     @Id
-    @GeneratedValue(
-            strategy = GenerationType.AUTO)
-    @Column(
-            name = "id")
-    private int               id;
-    @Column(
-            name = "city")
-    private String            city;
-    @Column(
-            name = "state")
-    private String            state;
-    @Column(
-            name = "zipcode")
-    private int               zipcode;
-    @Column(
-            name = "country")
-    private String            country;
-    @Column(
-            name = "street_name")
-    private String            street_name;
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private int id;
 
-    public Location() {
+    @Column(name = "city")
+    private String city;
 
-    }
+    @Column(name = "state")
+    private String state;
 
-    public int getId() {
-        return id;
-    }
+    @Column(name = "zipcode")
+    private int zipcode;
 
-    public void setId(int id) {
-        this.id = id;
-    }
+    @Column(name = "country")
+    private String country;
 
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getState() {
-        return state;
-    }
-
-    public void setState(String state) {
-        this.state = state;
-    }
-
-    public int getZipcode() {
-        return zipcode;
-    }
-
-    public void setZipcode(int zipcode) {
-        this.zipcode = zipcode;
-    }
-
-    public String getCountry() {
-        return country;
-    }
-
-    public void setCountry(String country) {
-        this.country = country;
-    }
-
-    public String getStreet_name() {
-        return street_name;
-    }
-
-    public void setStreet_name(String street_name) {
-        this.street_name = street_name;
-    }
-
+    @Column(name = "street_name")
+    private String street_name;
 }
--- ParkingSpaceReservation/src/main/java/com/parkingspace/models/ParkingLot.java
+++ ParkingSpaceReservation/src/main/java/com/parkingspace/models/ParkingLot.java
@@ -10,70 +10,32 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ParkingLot implements Serializable {
+
     /**
-     * 
+     *
      */
     private static final long serialVersionUID = 1L;
     @Id
-    @GeneratedValue(
-            strategy = GenerationType.AUTO)
-    @Column(
-            name = "id")
-    private int               id;
-    @Column(
-            name = "name")
-    private String            name;
-    @OneToOne(
-            cascade = CascadeType.ALL)
-    @JoinColumn(
-            name = "address_id",
-            referencedColumnName = "id")
-    private Location          address;
-    @Column(
-            name = "is_full")
-    private String            isFull;
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private int id;
 
-    public ParkingLot() {
-    }
+    @Column(name = "name")
+    private String name;
 
-    public ParkingLot(Location address) {
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "address_id", referencedColumnName = "id")
+    private Location address;
 
-        this.address = address;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Location getAddress() {
-        return address;
-    }
-
-    public void setAddress(Location address) {
-        this.address = address;
-    }
-
-    public String isFull() {
-        return isFull;
-    }
-
-    public void setFull(String isFull) {
-        this.isFull = isFull;
-    }
-
+    @Column(name = "is_full")
+    private String isFull;
 }
