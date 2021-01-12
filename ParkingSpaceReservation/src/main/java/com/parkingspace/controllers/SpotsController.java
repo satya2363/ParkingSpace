6e143844c32f4f8c574edbb39f76c5b2fd7608c3
Tharunya Reddy <tharunyapati@gmail.com>
2021-01-11 19:11:30 -0500

    Indent

--- ParkingSpaceReservation/src/main/java/com/parkingspace/controllers/SpotsController.java
+++ ParkingSpaceReservation/src/main/java/com/parkingspace/controllers/SpotsController.java
@@ -1,16 +1,12 @@
 package com.parkingspace.controllers;
 
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.parkingspace.models.Location;
-import com.parkingspace.models.ParkingLot;
 import com.parkingspace.models.ParkingSpot;
 import com.parkingspace.repositories.ParkingRepository;
 import com.parkingspace.services.QueryService;
@@ -22,10 +18,9 @@ public class SpotsController {
     ParkingRepository parkingRepository;
 
     @Autowired
-    QueryService      queryService;
+    QueryService queryService;
 
-    @GetMapping(
-            path = "/spotsByParkingLot")
+    @GetMapping(path = "/spotsByParkingLot")
     public @ResponseBody Iterable<ParkingSpot> getAvailableParkingSpots(@RequestParam int parkingLotId) {
         return queryService.spotQuery(parkingLotId);
     }
